81a8943eda1b2cc158bdd337e3a30e62c0a78981
byansanur <ratbyansanur81@gmail.com>
2022-06-28 23:16:16 +0700

    adding hilt dependencies, gitignore

--- app/src/main/java/com/salt/newsappsalt/SaltNewsApp.kt
+++ app/src/main/java/com/salt/newsappsalt/SaltNewsApp.kt
@@ -1,5 +1,7 @@
 package com.salt.newsappsalt
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class SaltNewsApp : Application()
--- app/src/main/java/com/salt/newsappsalt/presentation/MainActivity.kt
+++ app/src/main/java/com/salt/newsappsalt/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.salt.newsappsalt
+package com.salt.newsappsalt.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.salt.newsappsalt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
